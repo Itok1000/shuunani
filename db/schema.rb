469bf0f12d53a6ddd819edb9cf7f85ba0634dd20
伊藤健太 <jiantaiyiteng1@gmail.com>
2026-08-04 13:55:58 +0900

    環境構築

---
+++ db/schema.rb
@@ -0,0 +1,10 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the schema from scratch, use
+# bin/rails db:schema:load.
+
+ActiveRecord::Schema[8.1].define(version: 0) do
+end
