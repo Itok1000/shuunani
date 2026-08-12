ab01494c0991a83ccfe3f55bca8ac67709d63c39
伊藤健太 <jiantaiyiteng1@gmail.com>
2026-08-12 10:30:01 +0900

    診断機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,37 @@
+# app/controllers/posts_controller.rb
 class PostsController < ApplicationController
+  def new
+    # 診断フォーム表示
+  end
+
+  def create
+    # 送信された回答（q1〜q10）の合計と衛生スコアを計算
+    answers = params[:answers] || {}
+    total_score = answers.values.map(&:to_i).sum
+    hygiene_score = (1..3).sum { |i| answers["q#{i}"].to_i }
+
+    # 判定ロジック
+    result_type = calculate_result(total_score, hygiene_score)
+
+    # 本来はDB保存ですが、仮にセッションやURLパラメータで結果画面へ引き継ぐ例
+    redirect_to post_path(result_type)
+  end
+
+  def show
+    @result_type = params[:id] # 例: "shuu_nani", "kita_chan" など
+  end
+
+  private
+
+  def calculate_result(total, hygiene)
+    if total >= 40
+      hygiene >= 12 ? "nioi_nani" : "shuu_nani"
+    elsif total >= 25
+      "normal"
+    elsif total >= 15
+      "bocchi"
+    else
+      "kita_chan"
+    end
+  end
 end
